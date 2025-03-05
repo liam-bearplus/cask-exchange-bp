7f138d10353e01e080d5a0440f6a45a705b0f427
venn24 <venn@bear.plus>
2025-03-05 16:59:37 +0700

    update form clone

---
+++ src/components/shared/auth/credentials-signin-form-clone.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Form, FormField } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { signInDefaultValues } from "@/lib/constants";
+import { signInFormSchema } from "@/lib/validators";
+import { loginUser } from "@/services/auth";
+import { TLoginUser } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { signIn } from "next-auth/react";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { useForm } from "react-hook-form";
+
+const CredentialsSignInForm = () => {
+  const searchParams = useSearchParams();
+  const loginMutation = useMutation({
+    mutationKey: ["login"],
+    mutationFn: ({ data: data }: { data: TLoginUser }) => loginUser(data),
+  });
+  const onSubmit = (data: TLoginUser) => {
+    console.log("data", data);
+    loginMutation.mutate(
+      { data },
+      {
+        onSuccess: () => {
+          signIn("login", {
+            ...data,
+            redirect: true,
+            callbackUrl: "/",
+          });
+        },
+      }
+    );
+  };
+  const form = useForm({
+    resolver: zodResolver(signInFormSchema),
+    defaultValues: signInDefaultValues,
+  });
+  const SignInButton = () => {
+    return (
+      <Button
+        disabled={loginMutation.isPending}
+        className="w-full"
+        variant="default"
+      >
+        {loginMutation.isPending ? "Signing In..." : "Sign In"}
+      </Button>
+    );
+  };
+
+  return (
+    <Form {...form}>
+      <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <div>
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" {...field} />
+            </div>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <div>
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" {...field} />
+            </div>
+          )}
+        />
+        <div>
+          <SignInButton />
+        </div>
+        <div className="text-center text-sm text-muted-foreground">
+          Don&apos;t have an account?{" "}
+          <Link
+            href="/sign-up"
+            target="_self"
+            className="text-primary underline"
+          >
+            Sign up
+          </Link>
+          instead.
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default CredentialsSignInForm;
--- src/modules/signin/index.tsx
+++ src/modules/signin/index.tsx
@@ -1,14 +1,15 @@
-
 import CredentialsHead from "@/components/shared/auth/credentials-head";
-import CredentialsSignInForm from "@/components/shared/auth/credentials-signin-form";
+// import CredentialsSignInForm from "@/components/shared/auth/credentials-signin-form";
+import CredentialsSignInForm from "@/components/shared/auth/credentials-signin-form-clone";
 
 export default function SignInModule() {
-    return (
-        <div className="flex flex-col">
-            <CredentialsHead title="Sign In" />
-            <div className="pb-4">
-                <CredentialsSignInForm />
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-col">
+      <CredentialsHead title="Sign In" />
+      <div className="pb-4">
+        {/* <CredentialsSignInForm /> */}
+        <CredentialsSignInForm />
+      </div>
+    </div>
+  );
 }
