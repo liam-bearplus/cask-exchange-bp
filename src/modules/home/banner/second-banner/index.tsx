c53f6cb1bd78fe41fc39d221c1888cb038a3d53f
venn24 <venn@bear.plus>
2025-03-20 16:20:34 +0700

    update: tailwind desktop first

--- src/modules/home/banner/second-banner/index.tsx
+++ src/modules/home/banner/second-banner/index.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { TBannerItem } from "..";
 
 export const SecondaryBanner: React.FC<{ data: TBannerItem }> = ({ data }) => (
-    <div className="relative row-span-1 flex items-center overflow-hidden rounded-[0.625rem] p-[2rem]">
+    <div className="tablet:row-span-2 relative row-span-1 flex items-center overflow-hidden rounded-[0.625rem] p-[2rem]">
         <ImagePlaceholder
             src={data.imageSrc}
             alt="secondary-banner"
@@ -13,7 +13,7 @@ export const SecondaryBanner: React.FC<{ data: TBannerItem }> = ({ data }) => (
             className="absolute inset-0 -z-10"
         />
         <div className={`mt-auto flex flex-col ${data.maxWidthClass}`}>
-            <h2 className="mb-4 text-3xl font-medium text-white-main">
+            <h2 className="text-white-main mb-4 text-3xl font-medium">
                 {data.title}
             </h2>
             <Button
