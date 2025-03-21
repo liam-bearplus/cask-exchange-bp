bf2ff43add2610a188ba66d279f8eaaab5f494b2
venn24 <venn@bear.plus>
2025-03-21 18:28:29 +0700

    fix: login

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,10 @@ import { ROUTE_AUTH, ROUTE_PUBLIC } from "./lib/constants/route";
 
 export async function middleware(req: NextRequest) {
     const sessionToken = await getToken({ req });
-    const includesAuth = ROUTE_AUTH.LOGIN.includes(req.nextUrl.pathname);
+    const includesAuth = Object.values(ROUTE_AUTH).includes(
+        req.nextUrl.pathname
+    );
+
     if (sessionToken) {
         return NextResponse.redirect(ROUTE_PUBLIC.HOME);
     } else if (
